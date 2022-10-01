f933ef97b8f0e6e6020fa69ccd0817b4d7782ae3
zazezizozuzy <24947457+Zazezizozuzy@users.noreply.github.com>
2022-09-30 21:50:37 -0700

    Completed ship movement and added solar flare

---
+++ Solar.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Solar : Area2D
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+	private void _on_Solar_area_entered(object area)
+	{
+		// Replace with function body.
+	}
+}
